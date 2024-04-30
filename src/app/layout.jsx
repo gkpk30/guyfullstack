e78c484b2e673501731bec2a3e44aef0ae251115
gkpk30 <guykoomer@hotmail.com>
2024-04-30 16:52:21 -0700

    initial commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata = {
+  title: {
+    template: '%s - Guy Koomer',
+    default:
+      'Guy Koomer - Full Stack Developer, SEO, and Designer',
+  },
+  description:
+    'I’m Guy, a full stack developer and entrepreneur based in Los Angeles. I’m the founder and CEO of Salient Dynamics, where we develop strategies that empower small businesses to explore on-line growth on their own terms.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
