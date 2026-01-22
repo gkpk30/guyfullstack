e862d706ff63bf32161246e0bdd8eb102555e15d
gkpk30 <guykoomer@hotmail.com>
2026-01-22 11:24:33 -0800

    changed image order

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -335,7 +335,7 @@ function Photos() {
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image3, image10, image1, image4,image5].map((image, imageIndex) => (
+        {[image3, image1, image10, image5, image4].map((image, imageIndex) => (
           <div
             key={image.src}
             className={clsx(
