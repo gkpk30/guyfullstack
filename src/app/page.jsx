c39906c97fe336cde9576ce1767788cd240d1236
gkpk30 <guykoomer@hotmail.com>
2026-01-24 08:28:29 -0800

    changed subtext

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -408,8 +408,8 @@ export default async function Home() {
             Full Stack Developer and SEM Specialist.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Guy, a full stack developer and entrepreneur based in Los Angeles. I develop
-            strategies that empower small businesses to explore on-line growth on their
+            I’m Guy, a full stack developer in Los Angeles. I develop
+            strategies that empower local small businesses to explore on-line growth on their
             own terms.
           </p>
           <div className="mt-6 flex gap-6">
